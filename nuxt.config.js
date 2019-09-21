da63d35c70f990c0e97c443e299ca1314d1b6ed5
Vitaliy <app.vd@yandex.ru>
2019-09-21 11:47:33 +0300

    UPD

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Курс английского языка, работающий на 100% КПД' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
